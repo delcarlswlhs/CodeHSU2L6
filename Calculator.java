21d7429a68e4d064cd27b3ce56498a17578f05de
delcarlswlhs <113708789+delcarlswlhs@users.noreply.github.com>
2022-10-07 08:52:43 -0700

    printed out the average of 2 and 3 doubles; made them objects

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,13 @@
 public class Calculator {
-    public void calcAverage(int num1, double num2) {
+    public double calcAverage(int num1, double num2) {
         double average = (num1 + num2) / 2;
-        System.out.println("The average is " + average);
+        //System.out.println("The average is " + average);
+        return average;
     }
 
-    public void calcAverage(int num1, int num2, double num3) {
+    public double calcAverage(int num1, int num2, double num3) {
         double average = (num1 + num2 + num3) / 3;
-        System.out.println("The average is " + average);
+        //System.out.println("The average is " + average);
+        return average;
     }
 }
--- CalculatorTester.java
+++ CalculatorTester.java
@@ -1,7 +1,13 @@
 public class CalculatorTester {
     public static void main(String[] args) {
         Calculator newAverage = new Calculator();
-        newAverage.calcAverage(20, 15.4);
-        newAverage.calcAverage(5, 8, 6.4);
+        
+        double firstAverage = newAverage.calcAverage(20, 15.4);
+        //double value stored in firstAverage and can be used wherever you want
+        System.out.println("The average of 20 and 15.4 is "+ firstAverage);
+        
+        double secondAverage = newAverage.calcAverage(5, 8, 6.4);
+        System.out.println("The average of 5 and 8 and 6.4 is" + secondAverage);
+    
     }
 }
